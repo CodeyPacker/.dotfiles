1d544c5bf0650984b617d6ed0e4f3ff35c98c4d5
Cody Packer <cody.packer@clearlink.com>
2024-03-01 21:55:19 -0700

    Added more transparent theme support

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -33,16 +33,15 @@ return {
       vim.cmd.colorscheme("catppuccin")
 
       -- General transparency
-      vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
-      vim.cmd("highlight NonText guibg=NONE ctermbg=NONE")
-      vim.cmd("highlight NormalNC guibg=NONE ctermbg=NONE")
-
+      vim.cmd("highlight Normal guibg=#00000050")
+      vim.cmd("highlight NonText guibg=#00000050")
+      vim.cmd("highlight NormalNC guibg=#00000050")
       -- File tree transparency
-      vim.cmd("highlight NvimTreeNormal guibg=NONE ctermbg=NONE")
-      vim.cmd("highlight NvimTreeNormalNC guibg=NONE ctermbg=NONE")
+      vim.cmd("highlight NvimTreeNormal guibg=#00000099")
+      vim.cmd("highlight NvimTreeNormalNC guibg=#00000099")
       -- Additional elements
-      vim.cmd("highlight StatusLineNC guibg=NONE ctermbg=NONE")
-      vim.cmd("highlight VertSplit guibg=NONE ctermbg=NONE")
+      vim.cmd("highlight StatusLineNC guibg=#00000050")
+      vim.cmd("highlight VertSplit guibg=#00000050")
     end,
   },
 }
