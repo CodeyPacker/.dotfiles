140f9da9f1fedd543981c4e6b90d9ea3c591ab6d
Cody Packer <cody.packer@clearlink.com>
2024-03-06 20:36:50 -0700

    Swapping neo tree for nvim tree

---
+++ nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,43 @@
+return {
+  {"tpope/vim-commentary"}, -- existing plugin
+
+  -- nvim-tree with detailed configuration
+  {"kyazdani42/nvim-tree.lua", config = function()
+    -- Change color for arrows in tree to light blue
+    vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
+
+    require("nvim-tree").setup{
+      renderer = {
+        icons = {
+          glyphs = {
+            folder = {
+              arrow_closed = "", -- arrow when folder is closed
+              arrow_open = "", -- arrow when folder is open
+            },
+          },
+        },
+        group_empty = true, -- group empty folders together
+      },
+      actions = {
+        open_file = {
+          window_picker = {
+            enable = false, -- disable window_picker
+          },
+        },
+      },
+      filters = {
+        dotfiles = true, -- show dotfiles
+      },
+      view = {
+        width = 30, -- set the view width to 30
+      },
+      -- Uncomment the following if you want to enable git integration
+      -- git = {
+      --   ignore = false, -- show files ignored by git
+      -- },
+    }
+
+    -- Key mapping to toggle nvim-tree
+    vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
+  end},
+}
