143189518781c4549612932222198405780389d6
Cody Packer <cody.packer@clearlink.com>
2024-03-08 10:12:23 -0700

    Added zsh aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,3 +8,15 @@ alias dc="docker compose"
 alias dcr="dc run --rm"
 alias dcrp="dcr --service-ports --use-aliases"
 alias dce="dc exec"
+
+# Git 
+alias gcopy="git branch --show-current | tr -d '\n' | pbcopy"  # Copy current Git branch name to clipboard
+
+# Yarn aliases
+alias yc="yarn clean"
+alias yd="yarn develop"
+alias ycyd="yarn clean && yarn develop"
+alias ycys="yarn clean && yarn serve" 
+
+# Misc
+alias ll="ls -lAh"
